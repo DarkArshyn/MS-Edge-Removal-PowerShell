3a73a670d1017bf681c3f8da0e759af96411f8b1
DarkArshyn <8863727+DarkArshyn@users.noreply.github.com>
2024-06-18 15:11:31 +0200

    Update Uninstall Edge in PowerShell.ps1
    Update Edge Removal method (can now remove multiple version if installed)

--- Uninstall Edge in PowerShell.ps1
+++ Uninstall Edge in PowerShell.ps1
@@ -3,7 +3,7 @@
 #               Uninstall Edge in PowerShell - DarkArshyn              #
 #                       27/03/2024 - Version 1.1                       #
 #                                                                      #
-#                    Last revision : 07/05/2024                        #
+#                    Last revision : 18/06/2024                        #
 #                                                                      #
 ########################################################################
 
@@ -32,26 +32,46 @@ Write-Host "Removing Edge, please wait..."
 
 Try{
 
-    $ChildItem = Get-ChildItem ${env:ProgramFiles(x86)}\Microsoft\Edge\Application | Select Name
-    $VersionEdge = $ChildItem.Name | Select -First 1
-    Start-process "${env:ProgramFiles(x86)}\Microsoft\Edge\Application\$($VersionEdge)\Installer\setup.exe" "-uninstall --force-uninstall --system-level --delete-profile"
-    Remove-Item -Force -Recurse ${env:ProgramFiles(x86)}\Microsoft\Edge
+                    $ChildItem = Get-ChildItem ${env:ProgramFiles(x86)}\Microsoft\Edge\Application setup.exe -Recurse -Force
+                    $EdgeVersionDirectory = $ChildItem.DirectoryName
+                    $EdgeDirectory = $EdgeVersionDirectory|ForEach {$_ +  "\setup.exe"}
 
-    $GetAllEdgeApp = Get-AppxPackage -AllUsers | Where-Object { $_.PackageFullName -like '*microsoftedge*' } | Select-Object -ExpandProperty PackageFullName
-    $GetUsernameSID = Get-LocalUser -Name $env:USERNAME | Select Name,SID
-    ForEach ($EdgeApp in $GetAllEdgeApp) {
-        reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$($GetUsernameSID.SID.Value)\$($EdgeApp)" /f
-        reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\S-1-5-18\$($EdgeApp)" /f
-        reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\$($EdgeApp)" /f
-        Remove-AppxPackage -Package $EdgeApp -ErrorAction SilentlyContinue
-        Remove-AppxPackage -Package $EdgeApp -AllUsers -ErrorAction SilentlyContinue
-    }
+                    ps msedge | Stop-Process -Force
 
-    Clear-Host
-    Write-Host "Edge removal completed" -ForegroundColor Green
-}
-Catch{
+                    #If multiple version exists
+                    ForEach($EdgeVersion in $EdgeDirectory){
+                        Start-process $EdgeVersion "-uninstall --force-uninstall --system-level --delete-profile"
+                    }
+                    
+                    Remove-Item -Force -Recurse ${env:ProgramFiles(x86)}\Microsoft\Edge
 
-    Write-Host "Edge removal failed. An error has been detected : $($_.Exception.Message)." -ForegroundColor Red
+                    $GetAllEdgeApp = Get-AppxPackage -AllUsers | Where-Object { $_.PackageFullName -like '*microsoftedge*' } | Select-Object -ExpandProperty PackageFullName
+                    $GetUsernameSID = Get-LocalUser -Name $env:USERNAME | Select Name,SID
+                    ForEach ($EdgeApp in $GetAllEdgeApp) {
+                        reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\$($GetUsernameSID.SID.Value)\$($EdgeApp)" /f
+                        reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\EndOfLife\S-1-5-18\$($EdgeApp)" /f
+                        reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\$($EdgeApp)" /f
+                        $progressPreference = 'SilentlyContinue'
+                        Remove-AppxPackage -Package $EdgeApp -ErrorAction SilentlyContinue
+                        Remove-AppxPackage -Package $EdgeApp -AllUsers -ErrorAction SilentlyContinue
+                    }
 
-}
+                    If(Test-Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\*edge*.lnk"){
+                        Remove-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\*edge*.lnk"
+                    }
+                    If(Test-Path "$env:PUBLIC\Desktop\*edge*.lnk"){
+                        Remove-Item "$env:PUBLIC\Desktop\*edge*.lnk"
+                    }
+
+                    Stop-Process -ProcessName explorer -Force
+
+                    Start-Sleep 5
+
+                    Write-Host "Edge removal completed" -ForegroundColor Green
+
+                }
+                Catch{
+
+                    Write-Host "Edge removal failed. An error has been detected : $($_.Exception.Message)." -ForegroundColor Red
+
+                }
